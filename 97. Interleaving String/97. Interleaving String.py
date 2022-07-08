8e6f03516b5859ca6c22cb242cd3c3df43077032
Suyash Gupta <sgalpha01@gmail.com>
2022-07-08 07:18:26 +0530

    added 97

--- 97. Interleaving String/97. Interleaving String.py
+++ 97. Interleaving String/97. Interleaving String.py
@@ -16,8 +16,32 @@ def is_interleave(s1, s2, s3):
     return dfs(0, 0)
 
 
-s1 = "aabcc"
-s2 = "dbbca"
-s3 = "aadbbcbcac"
+def is_interleave_dp(s1, s2, s3):
+    m, n = len(s1), len(s2)
+    if m + n != len(s3):
+        return False
+    if n > m:
+        m, n = n, m
+        s1, s2 = s2, s1
+    dp = [False] * (n + 1)
+    dp[0] = True
+    for j in range(1, n + 1):
+        dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
+    for i in range(1, m + 1):
+        dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
+        for j in range(1, n + 1):
+            choose_s1, choose_s2 = False, False
+            if s1[i - 1] == s3[i + j - 1]:
+                choose_s1 = dp[j]
+            if s2[j - 1] == s3[i + j - 1]:
+                choose_s2 = dp[j - 1]
+            dp[j] = choose_s1 or choose_s2
 
-print(is_interleave(s1, s2, s3))
+    return dp[-1]
+
+
+s1 = "abad"
+s2 = "aabc"
+s3 = "aababacd"
+
+print(is_interleave_dp(s1, s2, s3))
